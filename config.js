b71b05806dfa9624beb731b5dab30ccc6fd087a2
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-01-19 02:02:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     BOT_TOKEN: "7981971726:AAFevWyY1TneRaI8w2BomrXa-Kf-NZXe0JU", 
     ADMIN_ID: 6625936112,                
-    CHANNEL: "@ll_Bot_Promotion_ll",   
+    CHANNEL: "@DmWinAi",   
 };
